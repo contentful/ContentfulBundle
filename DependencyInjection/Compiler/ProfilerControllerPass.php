401a1c989f0648c35f1e28cfd7572c0e4225324a
Tomasz Hanc <tomasz.hanc@gmail.com>
2016-08-08 14:44:26 +0200

    Register ProfilerController only if profiler service is defined

---
+++ DependencyInjection/Compiler/ProfilerControllerPass.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright 2016 Contentful GmbH
+ * @license   MIT
+ */
+
+namespace Contentful\ContentfulBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class ProfilerControllerPass implements CompilerPassInterface
+{
+    /**
+     * You can modify the container here before it is dumped to PHP code.
+     *
+     * @param ContainerBuilder $container
+     */
+    public function process(ContainerBuilder $container)
+    {
+        if (! $container->hasDefinition('profiler')) {
+            return;
+        }
+
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../../Resources/config'));
+        $loader->load('profiler-controller.xml');
+    }
+}
