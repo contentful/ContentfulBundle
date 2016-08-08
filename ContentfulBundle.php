ac656c25d0d43a26d0f1447b1dac01aabacd941e
Tomasz Hanc <tomasz.hanc@gmail.com>
2016-08-08 14:37:53 +0200

    Register ProfilerController only if profiler service is defined

--- ContentfulBundle.php
+++ ContentfulBundle.php
@@ -6,8 +6,14 @@
 
 namespace Contentful\ContentfulBundle;
 
+use Contentful\ContentfulBundle\DependencyInjection\Compiler\ProfilerControllerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ContentfulBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new ProfilerControllerPass());
+    }
 }
